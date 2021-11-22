3f80d23f51c8eb7cac92d652bdbd033c378ff2fe
Dandi Irwanto <57336670+dandi200600@users.noreply.github.com>
2021-11-22 22:03:52 +0700

    Update favRestaurantIdbSpec.js
    Change Movie to Restaurant in Describe func

--- specs/favRestaurantIdbSpec.js
+++ specs/favRestaurantIdbSpec.js
@@ -1,7 +1,7 @@
 import { itActsAsFavoriteRestaurantModel } from './contract/favRestaurantContract';
 import FavRestaurantIdb from '../src/scripts/data/restaurant-idb';
 
-describe('Favorite Movie Idb Contract Test Implementation', () => {
+describe('Favorite Restaurant Idb Contract Test Implementation', () => {
   afterEach(async () => {
     (await FavRestaurantIdb.getAllRestaurants()).forEach(async (restaurant) => {
       await FavRestaurantIdb.deleteRestaurant(restaurant.id);
